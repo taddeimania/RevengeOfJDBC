1d52395e360f4ad817182ab3db7aef6334c3ad5c
taddeimania <jtaddei@gmail.com>
2017-09-20 11:26:12 -0400

    Mostly completed database manager

--- src/main/java/io/joel/Main.java
+++ src/main/java/io/joel/Main.java
@@ -1,28 +1,26 @@
 package io.joel;
 
+import io.joel.helpers.DatabaseManager;
+import io.joel.models.Stat;
+
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
 
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:stats.db")) {
-            Statement statement = conn.createStatement();
-            statement.executeUpdate("DROP TABLE IF EXISTS stats;");
-            statement.executeUpdate("CREATE TABLE stats (id INTEGER PRIMARY KEY, name STRING, wins INTEGER, losses INTEGER );");
-            statement.executeUpdate("INSERT INTO stats (name, wins, losses) VALUES ('joel', 10, 3);");
-            ResultSet rs = statement.executeQuery("SELECT * FROM stats");
 
-            while(rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                int wins = rs.getInt("wins");
-                int losses = rs.getInt("losses");
+            DatabaseManager dbm = new DatabaseManager(conn);
+            dbm.dropStatTable();
+            dbm.createStatTable();
+            dbm.insertIntoStatTable("joel", 100, 40);
+            ArrayList<Stat> results = dbm.getStats();
 
-                System.out.printf("%s: %s %s %s\n", id, name, wins, losses);
-            }
 
         } catch (SQLException ex) {
+            ex.printStackTrace();
             System.out.println("We encountered a problem talking to the database");
         }
 
---
+++ src/main/java/io/joel/models/Stat.java
@@ -0,0 +1,15 @@
+package io.joel.models;
+
+public class Stat {
+    private int id;
+    private String name;
+    private int wins;
+    private int losses;
+
+    public Stat(int id, String name, int wins, int losses) {
+        this.id = id;
+        this.name = name;
+        this.wins = wins;
+        this.losses = losses;
+    }
+}
